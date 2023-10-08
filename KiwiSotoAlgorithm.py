5fbded95e1ce2d53f2fd91844278ab06ff6426ec
Andrew Salls <asallsschool@gmail.com>
2023-10-08 14:41:46 -0400

    Code actually runs now
    Still definitely doesn't work

--- KiwiSotoAlgorithm.py
+++ KiwiSotoAlgorithm.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 
-# note: v has size n (e.g. i in {0,...n})
-# 0_v corresponds to a vector of size 2^2l representing every possible string value for both strings
 def FeasibleTriplet(length, n):
     v = np.zeros((n, 2 ** (2 * length)))
 
@@ -11,7 +9,7 @@ def FeasibleTriplet(length, n):
     for i in range(2, n + 1):
         v_i = F(v[i - 1], v[i - 2], length)
         R = max(v_i - v[i - 1])
-        W = v_i + 2 * sum(R) - F(v_i + sum(R), v_i, length)
+        W = v_i + 2 * R - F(v_i + R, v_i, length)
         E = max(0, max(W))
 
         if R - E >= r - e:
@@ -23,7 +21,7 @@ def FeasibleTriplet(length, n):
 # note: b contains 2^2l entries, and is 1 where v1 and v2 start with the same character and 0 otherwise
 def F(v1, v2, length):
     return np.concatenate((np.ones(2**(2*length - 2)), np.zeros(2**(2*length - 1)), np.ones(2**(2*length - 2)))) \
-           + max(F_z(0, v1, v2, length), F_z(1, v1, v2, length))
+           + np.maximum(F_z(0, v1, v2, length), F_z(1, v1, v2, length))
 
 
 def F_z(z, v1, v2, length):
@@ -43,20 +41,22 @@ def F_z1(z, v, length):
         range1 = range(2 ** (2 * length - 1), 3 * (2 ** (2 * length - 2)))
 
     # st & 0xAAAAAAAA = only even bits of st, corresponding to bits in A
-    #    & 0x2AAAAAAA = even bits except for first, T(A)
     # st & 0x55555555 = only odd bits of st, corresponding to bits in B
-    #    & 0x15555555 = odd bits except for first, T(B)
     for st in range1:
         # For st = (A, B),
         # v[(A, T(B)0)] + v[(A, T(B)1)]
-        ATB0 = (st & 0xAAAAAAAA) | ((st & 0x15555555) << 2)
+        A = st & 0xAAAAAAAA
+        TB = (st & 0x55555555) & (1 << (length - 1))
+        ATB0 = A | (TB << 2)
         ATB1 = ATB0 | 0b1  # 0b1 <- the smallest bit in B is set to 1
         ret[st] = v[ATB0] + v[ATB1]  # if h(A) != h(B) and h(A) = z
 
     for st in range2:
         # For st = (A, B),
         # v[(T(A)0, B)] + v[(T(A)1, B)]
-        TA0B = (st & 0x55555555) | ((st & 0x2AAAAAAA) << 2)
+        TA = (st & 0xAAAAAAAA) & (1 << (length - 1))
+        B = st & 0x55555555
+        TA0B = (TA << 2) | B
         TA1B = TA0B | 0b10  # 0b10 <- the smallest bit in A is set to 1
         ret[st] = v[TA0B] + v[TA1B]  # if h(A) != h(B) and h(B) = z
 
@@ -74,7 +74,9 @@ def F_z2(z, v, length):
     for st in rangeF:
         # For st = (A, B),
         # v[(T(A)0, T(B)0)] + v[(T(A)0, T(B)1)] + v[(T(A)1, T(B)0)] + v[(T(A)1, T(B)1)]
-        TA0TB0 = ((st & 0x15555555) << 2) | ((st & 0x2AAAAAAA) << 2)
+        TA = (st & 0xAAAAAAAA) & (1 << (length - 1))
+        TB = (st & 0x55555555) & (1 << (length - 1))
+        TA0TB0 = (TA << 2) | (TB << 2)
         TA0TB1 = TA0TB0 | 0b1
         TA1TB0 = TA0TB0 | 0b10
         TA1TB1 = TA0TB0 | 0b11
@@ -84,4 +86,5 @@ def F_z2(z, v, length):
 
 
 if __name__ == '__main__':
-    print(FeasibleTriplet(2, 2))
+    (v, r, e) = FeasibleTriplet(6, 40)
+    print((v, r, e))
